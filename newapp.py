cea9f2bcf1d1ba33f64cd001043871f786a588bb
godikemahesh <69629676+godikemahesh@users.noreply.github.com>
2024-06-01 19:44:43 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -12,7 +12,7 @@ st.write("""Welcome to EAPCET college finder. We are here to help you to find th
 st.write("These are the Districts with codes  Below..👇")
 st.write("__"*30)
 
-data = pd.read_excel(r"C:\Users\mahes\Downloads\TSEAMCET2022firstphase.xlsx")
+data = pd.read_excel("TSEAMCET2022firstphase.xlsx")
 
 def code():
     dist=["Hyderabad","Medchal","Ranga Reddy","Kothagudem","Suryapeta","Warangal","Khammam","Medak","Sangareddy","Kamareddy","NIzamadad","Karimnagar","Siddipet","Jagtial","Peddapally","Rajanna Sircilla","Wanaparthy","Mahabubnagar","Hanmakonda","Nalgonda","Yadadri bhuvanagiri"]
