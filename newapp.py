bd8a31a166d27056f7c496c94a75eb66993915e0
godikemahesh <69629676+godikemahesh@users.noreply.github.com>
2024-06-08 22:40:33 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -14,6 +14,7 @@ st.write("Here the Districts with codes  Below..👇")
 st.write("__"*30)
 
 data = pd.read_csv("TSEAMCET2022firstphase.csv")
+data = pd.read_csv("ts-eamcet-2022-final-rank converted.csv")
 
 def code():
     dist=["Hyderabad","Medchal","Ranga Reddy","Kothagudem","Suryapeta","Warangal","Khammam","Medak","Sangareddy","Kamareddy","NIzamadad","Karimnagar","Siddipet","Jagtial","Peddapally","Rajanna Sircilla","Wanaparthy","Mahabubnagar","Hanmakonda","Nalgonda","Yadadri bhuvanagiri"]
@@ -24,14 +25,60 @@ def code():
 code()
 data.drop(["INST CODE", "BRANCH NAME", "COED", "TYPE", "YEAR OF ESTB", "TUITION FEE", "AFFILIATED"], axis=1,
           inplace=True)
+data1.drop(["INST CODE", "BRANCH NAME", "COED", "TYPE", "YEAR OF ESTB", "TUITION FEE", "AFFILIATED"], axis=1,
+          inplace=True)
+data1.columns = data.columns.str.lower()
 data.columns = data.columns.str.lower()
 st.write("Enter the required details to get the list of colleges.")
+def search1(name1,rank1,mycaste1,branch1,dist1):
+    st.write("*"*30)
+    st.write("<p style='text-align: left;color: yellow;'>ROUND- 2</p>", unsafe_allow_html=True)
+    c = 0
+    lst = []
+    lst2 = []
+    ind=[]
+    branch1 = branch1.upper()
+    dist1 = dist1.upper()
+    p=data1["place"]
+    d = data1["dist"]
+    b = data1["branch"]
+    inst = data1["institute name"]
+    ind2=[]
+
+    for i in range(len(data1["branch"])):
+        if str(b[i]) in branch1:
+            lst.append(i)
+        if str(d[i]) in dist1:
+            lst2.append(i)
+    for i in range(len(data1[mycaste1])):
+        if data1[mycaste1][i] >= rank1:
+            ind.append(data1[mycaste1][i])
+            ind=sorted(ind)
+
+    for j in range(len(ind)):
+        ind2.append(data1[data1[mycaste1]==ind[j]].index[0])
+
+    for i in ind2:
+        if dist1=="ALL":
+            if i in lst:
+                st.write(f"{inst[i]}, ({p[i]}) -- {b[i]}")
+                c=1
+        elif i in lst and i in lst2:
+            st.write(f"{inst[i]}, ({p[i]}) -- {b[i]}")
+            c = 1
+
+    if c == 0:
+        st.write("No colleges available for the specified criteria")
+
+
 def search(name,rank,mycaste, branch, dist):
     st.write("*" * 30)
     st.write(f"Name: {name.upper()}")       
     st.write(f"Rank: {rank} ")
     st.write(f"Catageory: {mycaste} ")
-    st.write(f"DIST: {dist.upper()}")       
+    st.write(f"DIST: {dist.upper()}") 
+    st.write("*" * 30)       
+    st.write("<p style='text-align: left;color: yellow;'>ROUND- 1</p>", unsafe_allow_html=True)
     c = 0
     lst = []
     lst2 = []
@@ -93,6 +140,7 @@ mycaste=caste+" "+gen
 btn=st.button("get colleges")
 if btn:
     search(name,rank, mycaste, branch, d)
+    search1(name,rank, mycaste, branch, d)
 
 st.write("__"*30)
 st.write("__"*30)
